7ffe922d335430fdcdf3d9e7719bbd9019cdacb4
booreda <ratr908@gmail.com>
2021-07-01 19:58:05 +0200

    fc

--- src/components/Checkout/Confirmation.jsx
+++ src/components/Checkout/Confirmation.jsx
@@ -6,7 +6,7 @@ const Confirmation = ({ orderInfo, orderError }) => {
     return (
       <div className="confirmation">
         <Typography variant="h5">Error: {orderError}</Typography>
-        <Button component={Link} variant="outlined" type="button" to="ecomv2/">
+        <Button component={Link} variant="outlined" type="button" to="/ecomv2">
           Back to home
         </Button>
       </div>
@@ -18,7 +18,7 @@ const Confirmation = ({ orderInfo, orderError }) => {
         Thank you {orderInfo.customer.firstname} {orderInfo.customer.lastname}{" "}
         for your purchase!
       </Typography>
-      <Button component={Link} variant="contained" type="button" to="/">
+      <Button component={Link} variant="contained" type="button" to="/ecomv2">
         Continue shopping
       </Button>
     </div>
